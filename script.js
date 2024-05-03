74a193887b62fe865a0dcf21838261f8950f7fa5
patovera <verapatricio9@gmail.com>
2024-05-02 23:32:36 -0300

    all

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
+document.addEventListener("DOMContentLoaded", () => {
+    createGrid();
+})
 
-for (let i = 1; i <= 256; i++){
-    let div = document.createElement("div");
-    div.style.width = "30px";
-    div.style.height = "30px";
-    div.style.border = "2px solid blue";
-    div.id = "box";
-    document.getElementById("container").appendChild(div);
+let grid = document.getElementById("grid");
+let gridSize = document.querySelector("input").value;
+const okButton = document.querySelector("button");
+let isDrawing = false;
+
+function clearGrid() {
+    while (grid.firstChild){
+        grid.removeChild(grid.firstChild);
+    }
 }
 
-function changeStyle(div){
-    div.style.background = "blue";
+function createGrid(size = 16) {
+    clearGrid();
+    for (let i = 0; i < size * size; i++){
+        const div = document.createElement("div");
+
+        div.addEventListener("mouseover", () => {
+            if (isDrawing){
+                div.style.backgroundColor = "blue";
+            }
+        })
+            
+        div.addEventListener("mousedown", () => {
+            isDrawing = true;
+        })
+
+        div.addEventListener("mouseup", () => {
+            isDrawing = false;
+        })
+        
+        div.classList.add("box");
+        div.style.aspectRatio = "1/1";
+        div.style.flexBasis = `calc( 100% / ${gridSize})`;
+        grid.appendChild(div);
+    }
 }
 
+okButton.addEventListener("click", () => {
+    gridSize = document.querySelector("input").value;
+    if (gridSize < 2 || gridSize > 100){
+        alert("You need to choose number between 2 and 100.");
+    } else {
+        createGrid(gridSize);
+    }
+})
 
-document.getElementById("box").addEventListener("mouseover", changeStyle);
